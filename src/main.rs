5c4c4fabc202fb5b819e6ba68f72b54d99733ba9
gitxandert <alexandertoth96@yahoo.com>
2026-01-09 21:45:52 -0500

    main: partition into cells

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use libc::{
 };
 
 fn main() {
-    let mut csv: Vec<String> = match load_csv() {
+    let mut cells: Cells = match load_csv() {
         Ok(file) => file,
         Err(e) => {
             eprintln!("{e}");
@@ -21,14 +21,15 @@ fn main() {
     raw_mode(true);
     set_w_h();
 
-    let mut c_info = CellsInfo::new();
+    let (max_w, max_h) = cells.xy();
+    let mut w_info = WinInfo::new(max_w, max_h);
     loop {
-        process_input(&mut c_info);
-        show_csv(&mut csv, &mut c_info);
+        process_input(&mut w_info);
+        show_csv(&mut cells, &mut w_info);
     }
 }
 
-fn load_csv() -> Result<Vec<String>, io::Error> {
+fn load_csv() -> Result<Cells, io::Error> {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         return Err(io::Error::new(ErrorKind::InvalidInput,
@@ -61,53 +62,63 @@ fn load_csv() -> Result<Vec<String>, io::Error> {
         .map(|s| s.to_string())
         .collect();
 
-    Ok(lines)
+    let x = lines[0].split(',').count();
+    let y = lines.len();
+    let mut cells = Cells::new(x, y);
+    for line in lines {
+        let row: Vec<String> = line
+            .split(',')
+            .map(|s| s.to_string())
+            .collect();
+        cells.push_row(row);
+    }
+
+    Ok(cells)
 }
 
-fn process_input(c_info: &mut CellsInfo) {
-    if let Some(c) = read_char() {
-        match c {
-            27 => {
-                if let Some(c2) = read_char() {
-                    if c2 == b'[' {
-                        if let Some(c3) = read_char() {
-                            match c3 {
-                                b'D' => { // left
-                                    c_info.w_offset_left(1);
-                                }
-                                b'C' => { // right
-                                    c_info.w_offset_right(1);
-                                }
-                                b'A' => { // up
-                                    c_info.h_offset_up(1);
-                                }
-                                b'B' => { // down
-                                    c_info.h_offset_down(1);
-                                }
-                                _ => (),
-                            }
-                        } else {}
+fn process_input(w_info: &mut WinInfo) {
+    let c = read_char().unwrap_or(0);
+    match c {
+        27 => {
+            let c2 = read_char().unwrap_or(0);
+            if c2 == b'[' {
+                let c3 = read_char().unwrap_or(0);
+                match c3 {
+                    b'D' => { // left
+                        w_info.w_offset_left(1);
                     }
-                } else {}
+                    b'C' => { // right
+                        w_info.w_offset_right(1);
+                    }
+                    b'A' => { // up
+                        w_info.h_offset_up(1);
+                    }
+                    b'B' => { // down
+                        w_info.h_offset_down(1);
+                    }
+                    _ => (),
+                }
             }
-            _ => (), // ignore
         }
-    } else {}
+        _ => (), // ignore
+    }
 }
 
 
-fn show_csv(csv: &mut Vec<String>, c_info: &mut CellsInfo) {
+fn show_csv(cells: &mut Cells, w_info: &mut WinInfo) {
     unsafe {
         let cur_w = *w_ptr();
         let cur_h = *h_ptr();
 
-        if c_info.changed(cur_w.clone(), cur_h.clone()) {
+        if w_info.changed(cur_w.clone(), cur_h.clone()) {
             let mut out = stdout();
 
-            write!(out, "\x1b[3J\x1b[H").unwrap();
+            write!(out, "\x1b[2J\x1b[H").unwrap();
+            // clear scrollback
+            write!(out, "\x1b[3J").unwrap();
             
-            let h_offset = c_info.h_offset;
-            let w_offset = c_info.w_offset;
+            let h_offset = w_info.h_offset;
+            let w_offset = w_info.w_offset;
             let rows = cur_h.saturating_sub(1);
             let cols = cur_w.saturating_sub(1);
 
@@ -115,22 +126,74 @@ fn show_csv(csv: &mut Vec<String>, c_info: &mut CellsInfo) {
             // t_row corresponds to the terminal's row
             let mut row = h_offset;
             let mut t_row = 0usize;
+            let widths = cells.widths.clone();
             for _ in 0..rows {
+                /*
+                * Each line needs to be formatted like so:
+                * | content(...) | content(...) | content(...) |
+                * so the total width of the screen needs to be portioned:
+                * 5 + n x (1 + 1 + cell_width + 1)
+                * if the cell at the end goes over, it is dropped
+                */
                 // move cursor to (row+1, col=1)
                 write!(out, "\x1b[{};1H\x1b[2K", t_row+1).unwrap();
 
                 let mut line: String = "".to_string();
-                if row < csv.len() {
-                    line = csv[row]
-                        .chars()
-                        .skip(w_offset)
-                        .take(cols)
-                        .collect();
+                // vec of cols
+                let mut v_cols = &Vec::new();
+                // always print col names
+                if t_row == 0 {
+                    line = "    ".to_string();
+                    v_cols = cells.get_row(0);
+                    
+                } else {
+                    // if reached cells.len(),
+                    // print XXXX instead of row number
+                    if row < cells.len() {
+                        // print in hexadecimal (space-saving)
+                        line = format!("{:04X}", row);
+                        v_cols = cells.get_row(row);
+                    } else {
+                        line = "XXXX| EOF".to_string();
+                    }
                 }
-
-                write!(out, "{line}").unwrap();
-                row += 1;
-                t_row += 1;
+                let mut idx = w_offset;
+                let row_len = v_cols.len();
+                if row_len > 0 && idx < row_len{
+                    loop {
+                        if idx < row_len - 1 {
+                            let width = widths[idx];
+                            if line.len() + width > cur_w {
+                                break;
+                            }
+                            let col = &v_cols[idx];
+                            let mut contents: String = "".to_string();
+                            let mut cell: String = "".to_string();
+                            if col.len() > width {
+                                contents = col
+                                    .chars()
+                                    .take(width - 3)
+                                    .collect();
+                                cell = format!("| {:<width$}... ", 
+                                    contents, width = width - 3);
+                            } else {
+                                contents = col
+                                    .chars()
+                                    .take(width)
+                                    .collect();
+                                cell = format!("| {:<width$} ",
+                                    contents, width = width);
+                            }
+                            line += &cell;
+                            idx += 1;
+                        } else {
+                            break;
+                        }
+                    } 
+                    write!(out, "\x1b[4m{line}\x1b[0m").unwrap();
+                    row += 1;
+                    t_row += 1;
+                }
             }
 
             write!(out, "\x1b[{};1H\x1b[2K", t_row+1).unwrap();
@@ -153,21 +216,63 @@ unsafe fn h_ptr() -> *mut usize {
     &raw mut HEIGHT
 }
 
-struct CellsInfo {
+struct Cells {
+    rows: Vec<Vec<String>>,
+    x: usize,
+    y: usize,
+    widths: Vec<usize>,
+    heights: Vec<usize>,
+}
+
+impl Cells {
+    fn new(x: usize, y: usize) -> Self {
+        let rows = Vec::<Vec<String>>::new();
+        let widths = vec![12usize; x];
+        let heights = vec![1usize; y];
+
+        Self { rows, x, y, widths, heights }
+    }
+
+    fn xy(&self) -> (usize, usize) {
+        (self.x, self.y)
+    }
+
+    fn push_row(&mut self, row: Vec<String>) {
+        self.rows.push(row);
+    }
+
+    fn get_row(&mut self, idx: usize) -> &Vec<String> {
+        if idx > self.y {
+            return &self.rows[self.y];
+        } else {
+            return &self.rows[idx];
+        }
+    }
+
+    fn len(&self) -> usize {
+        self.y
+    }
+}
+
+struct WinInfo {
     width: usize,
     height: usize,
     w_offset: usize,
     h_offset: usize,
+    max_w: usize,
+    max_h: usize,
     was_changed: bool,
 }
 
-impl CellsInfo {
-    fn new() -> Self {
+impl WinInfo {
+    fn new(max_w: usize, max_h: usize) -> Self {
         Self {
             width: 0usize, 
             height: 0usize,
             w_offset: 0usize,
             h_offset: 0usize,
+            max_w: max_w,
+            max_h: max_h,
             was_changed: false,
         }
     }
@@ -196,23 +301,31 @@ impl CellsInfo {
     }
 
     fn h_offset_up(&mut self, val: usize) {
-        self.h_offset = self.h_offset.saturating_sub(val);
-        self.was_changed = true;
+        if self.h_offset > 0 {
+            self.h_offset = self.h_offset.saturating_sub(val);
+            self.was_changed = true;
+        }
     }
 
     fn h_offset_down(&mut self, val: usize) {
-        self.h_offset += val;
-        self.was_changed = true;
+        if self.h_offset < self.max_h {
+            self.h_offset += val;
+            self.was_changed = true;
+        }
     }
 
     fn w_offset_left(&mut self, val: usize) {
-        self.w_offset = self.w_offset.saturating_sub(val);
-        self.was_changed = true;
+        if self.w_offset > 0 {
+            self.w_offset = self.w_offset.saturating_sub(val);
+            self.was_changed = true;
+        }
     }
 
     fn w_offset_right(&mut self, val: usize) {
-        self.w_offset += val;
-        self.was_changed = true;
+        if self.w_offset < self.max_w {
+            self.w_offset += val;
+            self.was_changed = true;
+        }
     }
 }
 
@@ -299,4 +412,3 @@ fn read_char() -> Option<u8> {
         Err(_) => None,
     }
 }
-
