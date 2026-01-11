58adef2d9e3c89bd1abafa9b5f219fbb084ec636
gitxandert <alexandertoth96@yahoo.com>
2026-01-11 05:58:35 -0500

    csv: fix highlightting bug and parse CSV with quotes

--- src/csv.rs
+++ src/csv.rs
@@ -8,23 +8,23 @@ use crate::terminal::{h_ptr, w_ptr, WinInfo};
 
 pub struct Cells {
     rows: Vec<Vec<String>>,
-    x: usize,
-    y: usize,
+    num_cols: usize,
+    num_rows: usize,
     widths: Vec<usize>,
     heights: Vec<usize>,
 }
 
 impl Cells {
-    fn new(x: usize, y: usize) -> Self {
+    fn new(num_cols: usize, num_rows: usize) -> Self {
         let rows = Vec::<Vec<String>>::new();
-        let widths = vec![12usize; x];
-        let heights = vec![1usize; y];
+        let widths = vec![12usize; num_cols];
+        let heights = vec![1usize; num_rows];
 
-        Self { rows, x, y, widths, heights }
+        Self { rows, num_cols, num_rows, widths, heights }
     }
 
     pub fn xy(&self) -> (usize, usize) {
-        (self.x, self.y)
+        (self.num_cols, self.num_rows)
     }
 
     fn push_row(&mut self, row: Vec<String>) {
@@ -32,16 +32,104 @@ impl Cells {
     }
 
     fn get_row(&mut self, idx: usize) -> &Vec<String> {
-        if idx > self.y {
-            return &self.rows[self.y];
+        if idx > self.num_rows - 1 {
+            return &self.rows[self.num_rows - 1];
         } else {
             return &self.rows[idx];
         }
     }
 
-    fn len(&self) -> usize {
-        self.y
+    fn num_cols(&self) -> usize {
+        self.num_cols
     }
+
+    fn num_rows(&self) -> usize {
+        self.num_rows
+    }
+}
+
+fn parse_by_newline(block: &str) -> Vec<String> {
+    let mut parsed = Vec::<String>::new();
+    
+    let mut line = String::new();
+    let mut is_quoted = false;
+    let mut saw_newline = false;
+    
+    for c in block.chars() {
+        match c {
+            '"' => {
+                is_quoted = !is_quoted;
+                line.push(c);
+            }
+            '\n' => {
+                // guard against consecutive newlines
+                if !saw_newline {
+                    saw_newline = true;
+                    if !is_quoted {
+                        parsed.push(line.clone());
+                        line.clear();
+                    }
+                }
+            }
+            _ => {
+                saw_newline = false;
+                line.push(c);
+            }
+        }
+    }
+
+    parsed
+}
+
+fn parse_by_comma(line: &str) -> Vec<String> {
+    let mut parsed = Vec::<String>::new();
+    
+    let mut cell = String::new();
+    let mut is_quoted = false;
+    let mut quote: Option<char> = None;
+
+    for c in line.chars() {
+        match c {
+            '"' => {
+                match quote {
+                    None => {
+                        quote = Some(c);
+                    }
+                    Some(q) if q == c => {
+                        quote = None;
+                    }
+                    Some(_) => {
+                        cell.push(c);
+                    }
+                }
+            }
+            ',' if quote.is_none() => {
+                parsed.push(cell.clone());
+                cell = String::new();
+            }
+            _ => cell.push(c),
+        }
+    }
+
+    parsed.push(cell);
+    parsed
+}
+
+fn parse_csv_into_cells(csv: String) -> Result<Cells, io::Error> {
+    let lines: Vec<String> = parse_by_newline(&csv);
+
+    let col_names: Vec<String> = parse_by_comma(&lines[0]);
+    let num_cols = col_names.len();
+    let num_rows = lines.len();
+    let mut cells = Cells::new(num_cols, num_rows);
+    cells.push_row(col_names);
+
+    for i in 1..num_rows {
+        let row: Vec<String> = parse_by_comma(&lines[i]);
+        cells.push_row(row);
+    }
+
+    Ok(cells)
 }
 
 pub fn load_csv() -> Result<Cells, io::Error> {
@@ -68,39 +156,21 @@ pub fn load_csv() -> Result<Cells, io::Error> {
                 ));
     }
 
-    let mut f = File::open(filename)?;
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
+    let mut file = fs::read_to_string(filename)?;
 
-    contents = contents.replace("\r\n", "\n");
-
-    let lines: Vec<&str> = contents.lines().collect();
-
-    let x = lines[0].split(',').count();
-    let y = lines.len();
-    let mut cells = Cells::new(x, y);
-    for line in lines {
-        let row: Vec<String> = line
-            .trim_end_matches('\r')
-            .split(',')
-            .map(|s| s.to_string())
-            .collect();
-        eprintln!("len = {}", row.len());
-        cells.push_row(row);
-    }
+    let cells = parse_csv_into_cells(file)?;
 
     Ok(cells)
 }
 
 pub fn show_csv(cells: &mut Cells, w_info: &mut WinInfo) {
     unsafe {
-        let cur_w = *w_ptr();
-        let cur_h = *h_ptr();
-
-        if w_info.changed(cur_w.clone(), cur_h.clone()) {
+        if w_info.changed() {
+            let cur_w = *w_ptr();
+            let cur_h = *h_ptr();
             let mut out = stdout();
 
-            write!(out, "\x1b[2J\x1b[H").unwrap();
+            write!(out, "\x1b[H\x1b[2J").unwrap();
             // clear scrollback
             write!(out, "\x1b[3J").unwrap();
             
@@ -117,8 +187,12 @@ pub fn show_csv(cells: &mut Cells, w_info: &mut WinInfo) {
             let orig_idx = w_offset;
             let mut idx = orig_idx;
 
+            // consistent row_len
+            let row_len = cells.num_cols();
+            let num_rows = cells.num_rows();
+
             let widths = cells.widths.clone();
-            for _ in 0..rows.min(cells.len() - h_offset) {
+            for _ in 0..rows.min(cells.num_rows() - h_offset) {
                 /*
                 * Each line needs to be formatted like so:
                 * | content(...) | content(...) | content(...) |
@@ -134,24 +208,23 @@ pub fn show_csv(cells: &mut Cells, w_info: &mut WinInfo) {
                 let mut v_cols = &Vec::new();
                 // always print col names
                 if t_row == 0 {
-                    line = "    ".to_string();
+                    line = "    |".to_string();
                     v_cols = cells.get_row(0);
                 } else {
-                    // if reached cells.len(),
+                    // if reached cells.num_rows(),
                     // print XXXX instead of row number
-                    if row < cells.len() {
+                    if row < cells.num_rows() {
                         // print in hexadecimal (space-saving)
-                        line = format!("{:04X}", row);
+                        line = format!("{:04X}|", row);
                         v_cols = cells.get_row(row);
                     } else {
                         line = "XXXX| EOF".to_string();
                     }
                 }
-                let row_len = v_cols.len();
 
+                // reset index for each row
                 idx = orig_idx;
                 if row_len > 0 && idx < row_len {
-                    // reset index for each row
                     loop {
                         if idx < row_len {
                             let width = widths[idx];
@@ -167,25 +240,24 @@ pub fn show_csv(cells: &mut Cells, w_info: &mut WinInfo) {
                                             .chars()
                                             .take(width - 3)
                                             .collect();
-                                        cell = format!("| {:<width$}... ", 
+                                        cell = format!(" {:<width$}... |", 
                                             contents, width = width - 3);
                                     } else {
                                         contents = col
                                             .chars()
                                             .take(width)
                                             .collect();
-                                        cell = format!("| {:<width$} ",
+                                        cell = format!(" {:<width$} |",
                                             contents, width = width);
                                     }
                                 }
-                                None => eprintln!("No cell"),
+                                None => cell = " !!!CSVERR!!! |".to_string(),
                             }
 
                             // highlight the current cell
                             if w_info.w_pointer == idx &&
                                w_info.h_pointer == row {
-                                let mut sub = "\x1b[7m".to_string() + &cell + "\x1b[0;4m";
-                                cell = sub;
+                                cell = "\x1b[7m".to_string() + &cell + "\x1b[27m";
                             }
                            
                             line += &cell;
@@ -202,7 +274,13 @@ pub fn show_csv(cells: &mut Cells, w_info: &mut WinInfo) {
             }
             w_info.set_x_page(idx, orig_idx);
             w_info.set_y_page(row, h_offset);
-            write!(out, "\x1b[{};1H\x1b[2K", t_row+1).unwrap();
+
+            let row_num_len = 5;
+            
+            // move cursor to bottom
+            write!(out, "\x1b[{};{}H\x1b[2K", cur_h, 1)
+                .unwrap();
+
             out.flush().unwrap();
         }
     }
