4d0cf739ce672663c32d8ffcbc5a430acd436d82
gitxandert <alexandertoth96@yahoo.com>
2026-01-11 09:29:14 -0500

    input: incorporate alt key

--- src/input.rs
+++ src/input.rs
@@ -5,7 +5,8 @@ use std::{
 };
 use libc::input_event;
 
-use crate::terminal::WinInfo;
+use crate::csv::Cells;
+use crate::terminal::{ScrollMode, WinInfo};
 
 // Linux keycodes
 //
@@ -27,10 +28,10 @@ pub const KEY_LEFTCTRL:     u16 = 29;
 pub const KEY_RIGHTCTRL:    u16 = 97;
 
 pub const KEY_LEFTSHIFT:    u16 = 42;
-pub const KEY_RIGHTSHFIT:   u16 = 54;
+pub const KEY_RIGHTSHIFT:   u16 = 54;
 
-pub const KEY_LEFT_ALT:     u16 = 56;
-pub const KEY_RIGHT_ALT:    u16 = 100;
+pub const KEY_LEFTALT:     u16 = 56;
+pub const KEY_RIGHTALT:    u16 = 100;
 
 // input funcitons
 //
@@ -39,7 +40,7 @@ pub fn find_kbd() -> Result<File, io::Error> {
     File::open("/dev/input/event3")
 } 
 
-pub fn process_input(w_info: &mut WinInfo, kbd: &mut File) {
+pub fn process_input(w_info: &mut WinInfo, kbd: &mut File, cells: &mut Cells) {
     let ev = match read_char(kbd) {
         Some(event) => event,
         None => return,
@@ -47,26 +48,41 @@ pub fn process_input(w_info: &mut WinInfo, kbd: &mut File) {
     if ev.type_ == EV_KEY {
         if ev.value == KEY_DEPRESSED {
             match ev.code {
-                KEY_LEFTCTRL | KEY_RIGHTCTRL => w_info.page(),
-                KEY_LEFTSHIFT | KEY_RIGHTSHFIT => w_info.shift(),
-                KEY_LEFT => w_info.w_offset_left(),
-                KEY_RIGHT => w_info.w_offset_right(),
+                KEY_LEFTALT | KEY_RIGHTALT => {
+                    w_info.set_mode(ScrollMode::Text);
+                }
+                KEY_LEFTSHIFT | KEY_RIGHTSHIFT => {
+                    w_info.set_mode(ScrollMode::Axis);
+                }
+                KEY_LEFTCTRL | KEY_RIGHTCTRL => {
+                    eprintln!("ctrl");
+                    w_info.set_mode(ScrollMode::Page);
+                }
+                KEY_LEFT => w_info.w_offset_left(cells),
+                KEY_RIGHT => w_info.w_offset_right(cells),
                 KEY_UP => w_info.h_offset_up(),
                 KEY_DOWN => w_info.h_offset_down(),
                 _ => (),
             }
         } else if ev.value == KEY_REPEAT {
             match ev.code {
-                KEY_LEFT => w_info.w_offset_left(),
-                KEY_RIGHT => w_info.w_offset_right(),
+                KEY_LEFT => w_info.w_offset_left(cells),
+                KEY_RIGHT => w_info.w_offset_right(cells),
                 KEY_UP => w_info.h_offset_up(),
                 KEY_DOWN => w_info.h_offset_down(),
                 _ => (),
             }
         } else if ev.value == KEY_RELEASED {
             match ev.code {
-                KEY_LEFTCTRL | KEY_RIGHTCTRL => w_info.unpage(),
-                KEY_LEFTSHIFT | KEY_RIGHTSHFIT => w_info.unshift(),
+                KEY_LEFTSHIFT | KEY_RIGHTSHIFT => {
+                    w_info.set_mode(ScrollMode::Cell);
+                }
+                KEY_LEFTCTRL | KEY_RIGHTCTRL => {
+                    w_info.set_mode(ScrollMode::Cell);
+                }
+                KEY_LEFTALT | KEY_RIGHTALT  => {
+                    w_info.set_mode(ScrollMode::Cell);
+                }
                 _ => (),
             }
         }
@@ -76,8 +92,9 @@ pub fn process_input(w_info: &mut WinInfo, kbd: &mut File) {
 #[inline(always)]
 fn read_char(kbd: &mut File) -> Option<input_event> {
     let mut buf = [0u8; mem::size_of::<input_event>()];
-    match kbd.read_exact(&mut buf) {
+    match kbd.read(&mut buf) {
         Err(_) => None,
+        Ok(0) => None,
         Ok(_) => {
             let ev = unsafe {
                 ptr::read_unaligned(buf.as_ptr() as *const input_event)
