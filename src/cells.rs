9b6f5af1a7f953a08a99266456c27d2592b847dc
gitxandert <alexandertoth96@yahoo.com>
2026-01-19 10:49:18 -0500

    split io from cells logic (src/cells.rs, src/csv_io.rs)

---
+++ src/cells.rs
@@ -0,0 +1,450 @@
+use std::io::{self, Error, ErrorKind, Read, Write, stdout};
+
+use crate::terminal::{WinChange, WinInfo};
+
+#[derive(Debug)]
+pub struct EscSeq {
+    pub seq: String,
+    pub start: usize,
+    pub len: usize,
+}
+
+impl EscSeq {
+    pub fn new() -> Self {
+        Self {
+            seq: String::new(),
+            start: 0usize,
+            len: 0usize,
+        }
+    }
+
+    pub fn clone(&self) -> Self {
+        Self {
+            seq: self.seq.clone(),
+            start: self.start,
+            len: self.len,
+        }
+    }
+
+    pub fn push_seq(&mut self, c: char) {
+        self.seq.push(c);
+    }
+
+    pub fn set_start(&mut self, c: char, i: usize) {
+        self.push_seq(c);
+        self.start = i;
+    }
+}
+
+#[derive(Debug)]
+pub struct Cell {
+    pub content: String,
+    pub escape_sequences: Vec<EscSeq>,
+    pub width: usize,
+    pub text_offset: usize,
+    pub is_focused: bool,
+}
+
+impl Cell {
+    pub fn new(content: &str) -> Self {
+        // store escapes and their indices
+        let mut esq = Vec::<EscSeq>::new();
+
+        // len keeps track of "real" len
+        // (i.e. the characters, not the formatting)
+        let mut real_len = 0usize;
+
+        let mut line = String::new();
+        let mut e = EscSeq::new();
+        
+        let mut x = false;
+        let mut is_csi = false;
+        let mut i = 0usize;
+        // check for escape sequences;
+        for c in content.chars() {
+            match c {
+                '\x1b' => {
+                    x = true;
+                    e.set_start(c, i);
+                    continue;
+                }
+                '[' if x => {
+                    is_csi = true;
+                    e.push_seq(c);
+                    continue;
+                }
+                '@'..='~' if x => {
+                    e.push_seq(c);
+                    esq.push(e.clone());
+                    e = EscSeq::new();
+
+                    x = false;
+                    is_csi = false;
+                    continue;
+                }
+                '\n' => {
+                    // skip new lines
+                    continue;
+                }
+                _ => {
+                    match x {
+                        false => {
+                            line.push(c);
+                            i += 1;
+                        }
+                        true => {
+                            e.push_seq(c);
+                            match is_csi {
+                                true => continue,
+                                false => {
+                                    esq.push(e.clone());
+                                    e = EscSeq::new();
+
+                                    x = false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        Self { 
+            content: line,
+            escape_sequences: esq,
+            width: 12usize,
+            text_offset: 0usize,
+            is_focused: false,
+        }
+    }
+
+    fn clone_escapes(&self) -> Vec<EscSeq> {
+        let mut copy = Vec::<EscSeq>::new();
+        for esc in &self.escape_sequences {
+            copy.push(esc.clone());
+        }
+
+        copy
+    }
+
+    pub fn clone(&self) -> Self {
+        Self { 
+            content: self.content.clone(),
+            escape_sequences: self.clone_escapes(),
+            width: self.width,
+            text_offset: self.text_offset,
+            is_focused: self.is_focused,
+        } 
+    }
+
+    pub fn len(&self) -> usize {
+        self.content.len()
+    }
+
+    pub fn set_content(&mut self, input: String) {
+        self.content = input;
+    }
+
+    pub fn set_focused(&mut self, focused: bool) {
+        self.is_focused = true;
+    }
+
+    // directly set it
+    pub fn set_text_offset(&mut self, val: usize) {
+        self.text_offset = val;
+    }
+
+    pub fn dec_text_offset(&mut self, val: usize) {
+        self.text_offset = self.text_offset.saturating_sub(val);
+    }
+
+    pub fn inc_text_offset(&mut self, val: usize) {
+        if self.len().saturating_sub(self.text_offset) > self.width {
+            self.text_offset += val;
+        } else {
+            self.text_offset = self.len().saturating_sub(self.width);
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Column {
+    pub id: Cell,
+    pub header: Cell,
+    pub cells: Vec<Cell>,
+    pub start: usize, // terminal col where this begins
+    pub width: usize,
+}
+
+impl Column {
+    pub fn new() -> Self {
+        Self {
+            id: Cell::new(""),
+            header: Cell::new(""),
+            cells: Vec::<Cell>::new(),
+            start: 0usize,
+            width: 12usize,
+        }
+    }
+
+    pub fn push_cell(&mut self, cell: Cell) {
+        self.cells.push(cell);
+    }
+
+    pub fn set_col_id(&mut self, id: &str) {
+        self.id = Cell::new(id);
+    }
+
+    pub fn set_header(&mut self, header: &str) {
+        self.header = Cell::new(header);
+    }
+
+    pub fn set_start(&mut self, st: usize) {
+        self.start = st;
+    }
+
+    pub fn set_width(&mut self, w: usize) {
+        self.width = w;
+        for cell in &mut self.cells {
+            cell.width = w;
+        }
+    }
+
+    pub fn col_width(&self) -> usize {
+        return self.width + 3 // + 3 for formatting
+                              // (not for use with row_idx)
+    }
+
+    pub fn get_cell(&mut self, idx: usize) -> &mut Cell {
+        self.cells.get_mut(idx).unwrap()
+    }
+
+    pub fn len(&self) -> usize {
+        self.cells.len()
+    }
+}
+
+pub struct Cells {
+    pub header: Vec<Cell>,
+    pub col_idx: Vec<Cell>,
+    pub row_idx: Column,
+    pub columns: Vec<Column>,
+    pub w_cell: (usize, usize),
+    pub changed: bool,
+    pub written: bool,
+}
+
+impl Cells {
+    pub fn new(header: &Vec<Cell>, col_idx: &Vec<Cell>, row_idx: Column, num_cols: usize) -> Self {
+        let columns = Vec::<Column>::with_capacity(num_cols);
+        let w_cell = (0usize, 0usize);
+        let changed = false;
+        let written = false;
+
+        Self { 
+            header: Self::clone_cell_row(header), 
+            col_idx: Self::clone_cell_row(col_idx), 
+            row_idx, 
+            columns, 
+            w_cell, 
+            changed, 
+            written 
+        }
+    }
+
+    pub fn clone_cell_row(row: &Vec<Cell>) -> Vec<Cell> {
+        let mut new_row = Vec::<Cell>::new();
+        for cell in row {
+            new_row.push(cell.clone());
+        }
+
+        new_row
+    }
+
+    pub fn changed(&mut self) -> bool {
+        let ret = self.changed;
+        self.changed ^= ret;
+
+        ret
+    }
+
+    pub fn set_column_width(&mut self, w: usize) {
+        let width = (3 > w) as usize * 3 + (w > 3) as usize * w;
+        let idx = self.w_cell.0; // w_cell is where the focus is
+        let mut column = self.columns.get_mut(idx).unwrap();
+        column.set_width(width);
+        self.changed = true;
+    }
+
+    pub fn set_w_cell(&mut self, col: usize, row: usize) {
+        self.w_cell = (col, row);
+    }
+
+    pub fn push_column(&mut self, col: Column) {
+        self.columns.push(col);
+    }
+
+    pub fn push_to_col(&mut self, col: usize, cell: Cell) {
+        self.columns[col].push_cell(cell);
+    }
+
+    pub fn get_column(&mut self, idx: usize) -> &mut Column {
+        self.columns.get_mut(idx).unwrap()
+    }
+
+    pub fn num_cols(&self) -> usize {
+        self.columns.len()
+    }
+
+    pub fn num_rows(&self) -> usize {
+        self.columns[0].len()
+    }
+
+    pub fn w_cell(&mut self) -> &mut Cell {
+        let mut col = self.columns.get_mut(self.w_cell.0).unwrap();
+        col.get_cell(self.w_cell.1)
+    }
+}
+
+pub fn show_csv(cells: &mut Cells, w_info: &mut WinInfo) {
+    match w_info.changed {
+        WinChange::Cell => {
+            // redraw the focused cell,
+            // with changed content,
+            // plus the rest of the line
+            //
+            // 1 line
+            let mut w_cell = cells.w_cell();
+
+        }
+        WinChange::Focus => {
+            // redraw the last focused cell,
+            // w/o highlighting,
+            // plus the rest of its line,
+            // and the new focused cell, 
+            // w/ highlighting,
+            // plus the rest of its line
+            //
+            // 2 lines
+        }
+        WinChange::ColWidth => {
+            // redraw the column whose width has changed,
+            // plus all columns after
+            //
+            // all lines
+        }
+        WinChange::Rows => {
+            // shift rows and row_idx,
+            // but no need to redraw header and col_idx
+            //
+            // all lines, except for header and col_idx
+        }
+        WinChange::Columns => {
+            // shift columns and col_idx,
+            // but no need to redraw row_idx
+            //
+            // all lines, but not the row_idx column
+        }
+        WinChange::Screen => {
+            // draw to the new terminal screen dimensions;
+            // if they shrink, 
+            // only maybe need to redraw focused content
+            //
+            /*
+            let mut flush = false;
+            if w_info.old_height > w_info.height {
+                // focused content always drawn at height, width
+                w_info.draw_focused_content();
+                flush = true;
+            } else {
+                // draw rows
+                // w_info.draw_focused_content();
+                // flush = true;
+            }
+
+            if w_info.width > w_info.old_width {
+                let mut start = w_info.old_width;
+                let orig = w_info.w_offset + w_info.w_page;
+                let mut idx = orig;
+                loop {
+                    let mut col = cells.get_column(idx + 1);
+                    let col_width = col.col_width();
+                    if start + col_width < w_info.width {
+                        w_info.draw_column(&mut col, start);
+                        start += col_width;
+                        idx += 1;
+                    } else {
+                        break;
+                    }
+                }
+                if idx != orig {
+                    w_info.set_w_page(
+                        w_info.w_offset, idx
+                    );
+                    flush = true;
+                }
+            }
+
+            if flush {
+                w_info.flush();
+            }
+            
+            w_info.changed = WinChange::Non;
+            */
+        }
+        WinChange::Init => { // first draw
+            // a blank cell and HEAD never change,
+            // so just write these now and once;
+            // also, start the underline that should never end
+            // (i.e. only write \x1b[0m or \x1b[24m
+            // before the focused content at the bottom)
+            //
+            w_info.push_to_frame("\x1b[1;1H");
+            w_info.push_to_frame("\x1b[4m    |");
+            w_info.push_to_frame("\x1b[2;1H");
+            w_info.push_to_frame("\x1b[1;30;47mHEAD \x1b[22;39;49m");
+
+            w_info.draw_row_idx(cells);
+
+            let mut id = 0usize;
+            let mut start = 6usize; // width of row_idx 
+                                    // + column to begin
+            loop {
+                let mut col = cells.get_column(id);
+                let col_width = col.col_width();
+
+                // set pointer to upperleftmost cell;
+                // ensures highlighting of this cell
+                if start == 6usize {
+                    let mut w_cell = col.get_cell(0);
+                    w_cell.set_focused(true);
+                }
+                
+                if start + col_width < w_info.width {
+                    w_info.draw_column(&mut col, start);
+                    start += col_width;
+                    id += 1;
+                } else {
+                    break;
+                }
+            }
+
+            // set w_cell to upperleftmost cell
+            cells.set_w_cell(0,0);
+
+            // offsets are at zero
+            w_info.set_w_page(
+                id, 0
+            );
+            // - 2 because of fixed col_ids and header
+            w_info.set_h_page(
+                w_info.height.saturating_sub(2), 0
+            );
+            
+            w_info.flush(); 
+        }
+        WinChange::Non => {
+            // do nothing
+        }
+    }
+}
---
+++ src/csv_io.rs
@@ -0,0 +1,384 @@
+use std::{
+    env,
+    fs::{self, File},
+    time::{SystemTime, UNIX_EPOCH},
+    io::{self, Error, ErrorKind, Read, Write, stdout},
+    path::{Component, Path, PathBuf}
+};
+
+use crate::cells::{Cell, Cells, Column, EscSeq};
+
+fn parse_by_newline(block: &str) -> Vec<String> {
+    let mut parsed = Vec::<String>::new();
+    
+    let mut line = String::new();
+    let mut is_quoted = false;
+    let mut saw_newline = false;
+    
+    for c in block.chars() {
+        match c {
+            '"' => {
+                is_quoted = !is_quoted;
+                line.push(c);
+            }
+            '\n' => {
+                // guard against consecutive newlines
+                if !saw_newline {
+                    saw_newline = true;
+                    if !is_quoted {
+                        parsed.push(line.clone());
+                        line.clear();
+                    }
+                }
+            }
+            _ => {
+                saw_newline = false;
+                line.push(c);
+            }
+        }
+    }
+
+    if !saw_newline {
+        parsed.push(line);
+    }
+
+    parsed
+}
+
+fn parse_by_delim(line: &str, delim: char) -> Vec<Cell> {
+    let mut parsed = Vec::<Cell>::new();
+    
+    let mut cell_str = String::new();
+    let mut is_quoted = false;
+   
+    let delim = match delim {
+        't' => '\t',
+        _ => delim
+    };
+
+    for c in line.chars() {
+        match c {
+            '"' => {
+                is_quoted = !is_quoted;
+            }
+            ch if ch == delim && ch != '"' && !is_quoted => {
+                let cell = Cell::new(&cell_str);
+                parsed.push(cell);
+                cell_str = String::new();
+            }
+            _ => cell_str.push(c),
+        }
+    }
+
+    let cell = Cell::new(&cell_str);
+    parsed.push(cell);
+    parsed
+}
+
+fn make_col_idx(num_cols: usize) -> Vec<Cell> {
+    let mut row = Vec::<Cell>::new();
+
+    let mut idx = "A".to_string();
+    for _ in 0..num_cols {
+        let cell = Cell::new(&idx);
+        row.push(cell);
+       
+        let mut i = 1;
+        let chars: String = idx.chars().rev().collect();
+        let mut add_a = false;
+        let mut inc_next = false;
+        let new_idx: String = chars
+            .chars()
+            .map(|c|
+                if c == 'Z' {
+                    if i == idx.len() {
+                        add_a = true;
+                    } else {
+                        inc_next = true;
+                    }
+                    i += 1;
+                     'A'
+                } else {
+                    let mut cc = c;
+                    if i == 1 || inc_next {
+                        let num = c as u32 + 1;
+                        cc = char::from_u32(num).unwrap();
+                        inc_next = false;
+                    }
+                    i += 1;
+                    cc
+                }
+            ).collect();
+
+        let new_idx: String = new_idx.chars().rev().collect();
+        if add_a {
+            idx = format!("{}A", new_idx);
+        } else {
+            idx = new_idx;
+        }
+    }
+
+    row
+}
+
+fn make_row_idx(len: usize) -> Column {
+    let mut row_idx = Column::new();
+    // row_idx always start at the first column
+    // and are four characters long
+    row_idx.set_start(0usize);
+    row_idx.set_width(4usize);
+
+    for i in 1..=len {
+        let index: String = format!("\x1b[30;47m{:04X}\x1b[39;49m", i);
+        row_idx.push_cell(Cell::new(&index));
+    }
+
+    row_idx
+}
+
+fn format_header(header: &mut Vec<Cell>) {
+    for i in 0..header.len() {
+        let content = &header[i].content;
+        header[i] = Cell::new(&format!("\x1b[30;47m{content}\x1b[0m"));
+    }
+}
+        
+
+fn parse_csv_into_cells(csv: String, delim: char) -> Result<Cells, io::Error> {
+    // extract lines, but parse into columns
+    let mut lines: Vec<String> = parse_by_newline(&csv);
+
+    let mut header: Vec<Cell> = parse_by_delim(&lines.remove(0), delim);
+    format_header(&mut header);
+    let col_len = header.len();
+    let col_idx: Vec<Cell> = make_col_idx(col_len);
+    
+    let row_idx: Column = make_row_idx(lines.len());
+    let mut cells = Cells::new(&header, &col_idx, row_idx, col_len);
+
+    for i in 0..lines.len() {
+        let row: Vec<Cell> = parse_by_delim(&lines[i], delim);
+        for j in 0..col_len {
+            if i == 0 {
+                let mut column = Column::new();
+                column.set_col_id(&col_idx[j].content);
+                column.set_header(&header[j].content);
+                cells.push_column(column);
+            }
+            let cell = row.get(j).unwrap().clone();
+            cells.push_to_col(j, cell);
+        }
+    }
+
+    Ok(cells)
+}
+
+pub fn load_csv(filename: String, delim: char) -> Result<Cells, io::Error> {
+    let mut file = fs::read_to_string(filename.clone())?;
+    // don't parse carriage returns
+    file = file.replace("\r", " ");
+
+    let cells = parse_csv_into_cells(file, delim)?;
+
+    Ok(cells)
+}
+
+pub fn save_backup(file: String) -> Result<(), io::Error> {
+    let content = fs::read_to_string(&file)?;
+    // find home dir or current dir; propagate error if neither
+    let home_dir = match env::home_dir() {
+        Some(path) => path,
+        None => match env::current_dir() {
+            Ok(path) => path,
+            Err(e) => return Err(e),
+        }
+    };
+
+    let file = Path::new(&file);
+    let abs = if file.is_absolute() {
+        file.to_path_buf()
+    } else {
+        env::current_dir()?.join(file)
+    };
+
+    let abs = fs::canonicalize(&abs)?;
+
+    let parent = abs.parent().ok_or_else(|| {
+        io::Error::new(
+            io::ErrorKind::NotFound, "Could not find parent dir"
+        )
+    })?;
+
+    let stem = abs.file_stem().ok_or_else(|| {
+        io::Error::new(
+            io::ErrorKind::NotFound, "Could not find file stem"
+        )
+    })?;
+
+    let rel_parent: PathBuf = parent
+                .components()
+                .filter_map(|c| match c {
+                    Component::Normal(p) => Some(p),
+                    _ => None,
+                })
+                .collect();
+
+    // either open or create backup directory
+    let dir_path = Path::new(&home_dir)
+        .join(".csview")
+        .join("backups")
+        .join(rel_parent)
+        .join(stem);
+
+    fs::create_dir_all(&dir_path)?;
+
+    let backup_dir = fs::read_dir(&dir_path)?;
+    let mut backup_files: Vec<_> = backup_dir
+        .filter_map(|res| res.ok())
+        .map(|e| e.path())
+        .collect();
+
+    // limit files in backup dir to 10
+    let mut dir_len = backup_files.len();
+    let mut stage_for_removal = Vec::<_>::new();
+    for bf in &backup_files {
+        if dir_len >= 10 {
+            stage_for_removal.push(bf.clone());
+            dir_len = dir_len.saturating_sub(1);
+        } else {
+            break;
+        }
+    }
+
+    // set default to the name of the youngest file,
+    // with timestamp incremented by 1
+    let default = match backup_files.pop() {
+        Some(backup) => {
+            let stem = backup.file_stem().unwrap_or_default();
+            let ext = backup.extension().unwrap_or_default();
+
+            let stem_str = stem.to_string_lossy();
+            let ext_str = ext.to_string_lossy();
+
+            let mut parts: Vec<&str> = stem_str.split('_').collect();
+            let prefix = parts[0];
+            let timestamp_str = match parts.pop() {
+                Some(ts) => ts,
+                None => "0",
+            };
+            let timestamp: u64 = match timestamp_str.parse() {
+                Ok(val) => val,
+                Err(_) => 0u64,
+            };
+            let faux_new = timestamp + 1;
+
+            let path = format!("{}_{}.{}", prefix, faux_new, ext_str);
+
+            dir_path.join(path)
+        }
+        // if no previous file, just make it
+        // {filename}_0(.ext)
+        None => {
+            let stem = file.file_stem().unwrap_or_default();
+            let ext = file.extension().unwrap_or_default();
+
+            let stem_str = stem.to_string_lossy();
+            let ext_str = ext.to_string_lossy();
+
+            let mut parts: Vec<&str> = stem_str.split('.').collect();
+            let prefix = parts[0];
+            let path = match parts.get(0) {
+                Some(ext) => format!("{}_0.{}", prefix, ext),
+                None => format!("{}_0", prefix),
+            };
+
+            dir_path.join(path)
+        }
+    };
+
+    // try to get timestamp to affix to file stem
+    let time = SystemTime::now().duration_since(UNIX_EPOCH);
+    let backup = match time {
+        Ok(t) => {
+            let stem = file.file_stem().unwrap_or_default();
+            let ext = file.extension().unwrap_or_default();
+
+            let prefix = stem.to_string_lossy();
+            let ext = ext.to_string_lossy();
+
+            let timestamp = t.as_secs();
+
+            let path = format!("{}_{}.{}", prefix, timestamp, ext);
+
+            dir_path.join(path)
+        }
+        Err(_) => default,
+    };
+
+    // remove old backups only if write succeeds
+    match fs::write(&backup, content) {
+        Ok(()) => {
+            println!("Wrote backup to {:?}", backup);
+            for path in stage_for_removal {
+                eprintln!("removing");
+                match fs::remove_file(path) {
+                    Ok(()) => (),
+                    Err(e) => eprintln!("{e}"),
+                }
+            }
+        }
+        Err(e) => return Err(e),
+    }
+
+    Ok(())
+}
+
+// TODO: reintroduce escape sequences
+pub fn write_to_file(mut cells: Cells, filename: String, delim: char) {
+    let mut sheet = String::new();
+    
+    { 
+        let header = &cells.header;
+        for i in 0..header.len() {
+            let cell = header.get(i).unwrap();
+            if cell.content.contains(delim) {
+                sheet.push_str(&format!("\"{}\"", cell.content));
+            } else {
+                sheet.push_str(&cell.content);
+            }
+
+            if i != header.len() - 1 {
+                sheet.push(delim);
+            }
+        }
+        sheet.push('\n');
+    }
+
+    for i in 0..cells.num_rows() {
+        let mut row = String::new();
+        for j in 0..cells.num_cols() {
+            let column = cells.get_column(i);
+            let cell = column.get_cell(j);
+            if cell.content.contains(delim) {
+                row.push_str(&format!("\"{}\"", cell.content));
+            } else {
+                row.push_str(&cell.content);
+            }
+            if j != cells.num_cols() - 1 {
+                row.push(delim);
+            }
+        }
+
+        if i != cells.num_rows() - 1 {
+            row.push('\n');
+        }
+
+        sheet.push_str(&row);
+    }
+    
+    match fs::write(&filename, sheet) {
+        Ok(()) => println!("Wrote {} to file", filename),
+        Err(e) => eprintln!("{e}"),
+    }
+}
+
