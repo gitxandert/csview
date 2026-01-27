dd1a36ff0eb7bf47f06e75da1c9b2f05f976abba
gitxandert <alexandertoth96@yahoo.com>
2026-01-27 16:39:03 -0500

    cmd_err: add errors for

--- src/cmd_err.rs
+++ src/cmd_err.rs
@@ -2,9 +2,13 @@ use std::io::{self, Read, Write};
 
 pub enum CmdErr {
     InvalidCommand,
+    MissingLocation,
     MissingName,
+    MissingSubCmd,
+    NoId,
     NoName,
     UnknownSpec,
+    UnmatchedQuote
 }
 
 pub fn print(err: CmdErr, token: &str, line: usize) {
@@ -17,8 +21,23 @@ pub fn print(err: CmdErr, token: &str, line: usize) {
                     line, token
                 ),
 
+            CmdErr::MissingLocation => format!(
+                    "\x1b[{};1H\x1b[2KERR: missing location for '{}'",
+                    line, token
+                ),
+
             CmdErr::MissingName => format!(
                     "\x1b[{};1H\x1b[2KERR: missing name for '{}'",
+                    line, token
+                ),
+
+            CmdErr::MissingSubCmd => format!(
+                    "\x1b[{};1H\x1b[2KERR: missing sub-command for '{}'",
+                    line, token
+                ),
+
+            CmdErr::NoId => format!(
+                    "\x1b[{};1H\x1b[2KERR: no column id '{}'",
                     line, token
                 ),
 
@@ -29,6 +48,11 @@ pub fn print(err: CmdErr, token: &str, line: usize) {
 
             CmdErr::UnknownSpec => format!(
                     "\x1b[{};1H\x1b[2KERR: unknown specifier '{}'",
+                    line, token
+                ),
+
+            CmdErr::UnmatchedQuote => format!(
+                    "\x1b[{};1H\x1b[2KERR: unmatched quote in {}",
                     line, token
                 ),
         }
