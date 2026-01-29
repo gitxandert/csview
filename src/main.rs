6d27bf5f5f9e2d6666cc17bf09660dceb8ef6a43
gitxandert <alexandertoth96@yahoo.com>
2026-01-29 12:14:22 -0500

    main: adjust writes to catch Results

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ use crate::{
 fn main() {
     let mut args = env::args();
     if args.len() < 2 {
-        eprintln!("csview expects at least a file name");
+        println!("csview expects at least a file name");
         return;
     }
 
@@ -32,7 +32,7 @@ fn main() {
                 match args.next() {
                     Some(d) => {
                         if d.len() > 1 {
-                            eprintln!("Argument to -d/--delimiter must be a single character t for tab)");
+                            println!("Argument to -d/--delimiter must be a single character t for tab)");
                             return;
                         }
                         if d == "t" {
@@ -42,7 +42,7 @@ fn main() {
                         }
                     }
                     None => {
-                        eprintln!("No argument provided for -d/--delimiter");
+                        println!("No argument provided for -d/--delimiter");
                         return;
                     }
                 }
@@ -54,7 +54,7 @@ fn main() {
     let mut cells: Cells = match load_csv(filename.clone(), delimiter.clone()) {
         Ok(file) => file,
         Err(e) => {
-            eprintln!("{e}");
+            println!("{e}");
             return;
         }
     };
@@ -104,12 +104,18 @@ fn main() {
                     // save backup to 
                     // /home/user/.csview/backups/;
                     // if fails, don't write
-                    match save_backup(filename.clone()) {
-                        Ok(()) => write_to_file(cells, filename, delimiter),
+                    match save_backup(&cells.filename) {
+                        Ok(b) => {
+                            println!("{b}");
+                            match write_to_file(&mut cells) {
+                                Ok(s) => println!("{s}"),
+                                Err(e) => println!("{e}"),
+                            }
+                        }
                         Err(e) => {
-                            eprintln!("WARNING -- could not create back up due to the following:");
-                            eprintln!("\n\t{e}");
-                            eprintln!("Exiting without writing");
+                            println!("WARNING -- could not create back up due to the following:");
+                            println!("\n\t{e}");
+                            println!("Exiting without writing");
                     }
                 }
             }
